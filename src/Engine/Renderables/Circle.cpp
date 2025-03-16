0e7cf5ffe89975187ff2a0b63d3bd9eba13b8d6b
Seb <seb1rblx@gmail.com>
2025-03-16 10:24:38 +0100

    Add Circle renderable

---
+++ src/Engine/Renderables/Circle.cpp
@@ -0,0 +1,31 @@
+#include "precomp.h"
+#include "Circle.h"
+#include "LineSegment.h"
+
+Engine::Circle::Circle( const float2& center, float size, uint color, int segmentCount ) :
+	m_center(center), m_size(size), m_color(color), m_segmentCount(segmentCount)
+{
+}
+
+void Engine::Circle::Render( const Camera& camera, Surface& drawSurface )
+{
+	RenderWorldPos(camera, drawSurface, m_center, m_size, m_color, m_segmentCount);
+}
+
+void Engine::Circle::RenderWorldPos( const Camera& camera, Surface& drawSurface, const float2& center, float size, uint color, int segmentCount )
+{
+	const float segmentCountF = static_cast<float>(segmentCount);
+
+	for (int i = 0; i < segmentCount; i++)
+	{
+		const float iF = static_cast<float>(i);
+
+		const float angle1 = (2.0f * PI * iF) / segmentCountF;
+		const float angle2 = (2.0f * PI * (iF + 1.f)) / segmentCountF;
+
+		float2 p1 = center + float2{size * cos(angle1), size * sin(angle1)};
+		float2 p2 = center + float2{size * cos(angle2), size * sin(angle2)};
+
+		Engine::LineSegment::RenderWorldPos(camera, drawSurface, p1, p2, color);
+	}
+}
