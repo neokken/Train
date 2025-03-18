c081fde3d3be16e80477f1e10c4d71bee14e6a33
Okke <okkeroelofsen@gmail.com>
2025-03-18 22:56:33 +0100

    Add helper functions on trackManager, fixed some segment creation issues

--- src/Game/TrainSystem/TrackManager.h
+++ src/Game/TrainSystem/TrackManager.h
@@ -10,12 +10,18 @@ public:
 	TrackManager() = default;
 
 	TrackNodeID CreateNode( const float2& position );
-	TrackSegmentID CreateSegment( TrackNodeID nodeA, TrackNodeID nodeB );
+	TrackSegmentID CreateSegment( TrackNodeID nodeA_ID, TrackNodeID nodeB_ID );
 
 	void ConnectSegments( TrackSegmentID segmentA_ID, TrackSegmentID segmentB_ID, bool twoWayConnection = true );
 
+	[[nodiscard]] bool DoesNodeExists( TrackNodeID id ) const;
+	[[nodiscard]] bool DoesSegmentExists( TrackSegmentID id ) const;
+
 	[[nodiscard]] const TrackNode& GetTrackNode( TrackNodeID id ) const;
 	[[nodiscard]] const TrackSegment& GetTrackSegment( TrackSegmentID id ) const;
+
+	[[nodiscard]] TrackSegmentID GetNextSegmentPositive( TrackSegmentID id ) const;
+	[[nodiscard]] TrackSegmentID GetNextSegmentNegative( TrackSegmentID id ) const;
 
 	// saving & loading data
 	[[nodiscard]] nlohmann::json SerializeData() const;
@@ -24,8 +30,8 @@ public:
 private:
 	//nobody outside this class should touch nodes/segments
 	// if there is a case handle it differently...
-	[[nodiscard]] TrackNode& GetTrackNode( TrackNodeID id );
-	[[nodiscard]] TrackSegment& GetTrackSegment( TrackSegmentID id );
+	[[nodiscard]] TrackNode& GetMutableTrackNode( TrackNodeID id );
+	[[nodiscard]] TrackSegment& GetMutableTrackSegment( TrackSegmentID id );
 
 private:
 	Engine::IDGenerator<TrackNodeID> m_nodeIDGenerator;
