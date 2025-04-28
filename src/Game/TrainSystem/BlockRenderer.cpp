e09a968deb5a774056bba7770fcff3c035664760
Twenmod <Twengames@gmail.com>
2025-04-28 20:14:11 +0200

    block direction rendering

--- src/Game/TrainSystem/BlockRenderer.cpp
+++ src/Game/TrainSystem/BlockRenderer.cpp
@@ -29,12 +29,17 @@ void BlockRenderer::Render( Engine::Camera& camera )
 			const TrackSegment& startSegment = m_trackManager->GetTrackSegment(startSig.segment);
 			Engine::CurveData curve{startSegment.nodeA_Position, startSegment.nodeA_Direction, startSegment.nodeB_Position, startSegment.nodeB_Direction};
 			float2 lStart = Engine::CurvedSegment::GetPositionOnCurvedSegment(startSig.distanceOnSegment, curve);
+			float2 startDir = Engine::CurvedSegment::GetDirectionOnCurvedSegment(startSig.distanceOnSegment, curve);
+			float2 startDirRight = float2(startDir.y, -startDir.x);
+			if (!startSig.directionTowardsNodeB) startDir *= -1, startDirRight *= -1;
 			for (const auto& signal : connectionList.second)
 			{
 				const Signal& sig = m_signalManager->GetSignal(signal);
 				const TrackSegment& segment = m_trackManager->GetTrackSegment(sig.segment);
 				Engine::CurveData curve2{segment.nodeA_Position, segment.nodeA_Direction, segment.nodeB_Position, segment.nodeB_Direction};
 				float2 lEnd = Engine::CurvedSegment::GetPositionOnCurvedSegment(sig.distanceOnSegment, curve2);
+				Engine::LineSegment::RenderWorldPos(camera, (lStart + startDir * 0.3f) + startDirRight * 0.1f, (lStart) + startDirRight * 0.4f, color, HeightLayer::Debug, 0.05f);
+				Engine::LineSegment::RenderWorldPos(camera, (lStart) + startDirRight * 0.4f, (lStart + startDir * 0.3f) + startDirRight * .7f, color, HeightLayer::Debug, 0.05f);
 				Engine::LineSegment::RenderWorldPos(camera, lStart, lEnd, color, HeightLayer::Debug);
 			}
 		}
